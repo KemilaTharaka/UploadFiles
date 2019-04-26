c00319b42682b40ad52b6b39d4149c4d9bab0c77
Umayangana Wijenayake <umayangana3357@gmail.com>
2019-04-26 12:28:01 +0530

    update..

--- script.php
+++ script.php
@@ -1,14 +1,28 @@
-<?php
-$mysqli = new mysqli('localhost','root','','images') or die($mysqli->connect_error);
+<!-- $mysqli = new mysqli('localhost','root','','images') or die($mysqli->connect_error);
 $table = 'upload';
-
 $extentions = array('jpg','png','gif','jpeg');
-
 $file_ext = end($file_ext);
-
-
 $file_ext = end($file_ext);
-
 header('Access-Control-Allow-Origin: *');
+var_dump($_FILES); -->
 
-var_dump($_FILES);
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "images";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "INSERT INTO upload (name, img_dir)
+VALUES ('John', 'Doe')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
